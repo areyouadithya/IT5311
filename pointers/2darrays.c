ac45545588009b90db21b310bce54e50f26a98cb
areyouadithya <112653455+areyouadithya@users.noreply.github.com>
2022-12-13 05:31:07 +0530

    jjj

--- pointers/2darrays.c
+++ pointers/2darrays.c
@@ -3,4 +3,9 @@
 void main(){
     int arr[2][3]={{1,2,3},{4,5,6}};
     printf("%d\n",arr);
+    printf("%d\n",&arr[0][0]);
+    printf("%d\n",arr[0]);
+    printf("%d\n",*arr);
+    printf("%d\n",&arr[0]); 
+    //all of the above give the same
 }
